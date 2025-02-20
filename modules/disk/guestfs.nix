ccf34e22102ac96ac55f768a3fd0f66e060cdcba
Adam McKinley <adam@awmckinley.net>
2025-02-20 00:05:41 -0600

    fix: restrict libguestfs to Linux

--- modules/disk/guestfs.nix
+++ modules/disk/guestfs.nix
@@ -1,5 +1,11 @@
-{ pkgs, ... }:
 {
+  isLinux,
+  lib,
+  pkgs,
+  ...
+}:
+{ }
+// lib.optionalAttrs isLinux {
   environment.systemPackages = with pkgs; [
     # tools for accessing and modifying virtual machine disk images
     libguestfs-with-appliance
