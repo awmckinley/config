806f9bd615bcde3eaa573fd62eedd7315e7cf5e6
Adam McKinley <adam@awmckinley.net>
2025-02-20 19:38:12 -0600

    feat: update packages

--- modules/chat/discord.nix
+++ modules/chat/discord.nix
@@ -1,6 +1,7 @@
 {
   isDarwin,
   isLinux,
+  isWSL,
   lib,
   pkgs,
   ...
@@ -15,7 +16,7 @@
     "overlayed"
   ];
 }
-// lib.optionalAttrs isLinux {
+// lib.optionalAttrs (isLinux && !isWSL) {
   environment.systemPackages = with pkgs; [
     # all-in-one cross-platform voice and text chat for gamers
     discord
