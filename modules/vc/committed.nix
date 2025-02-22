535fcec2e71505da3e763d0f8d301eddeebe09e5
Adam McKinley <adam@awmckinley.net>
2025-02-21 19:16:29 -0600

    feat: install committed on Linux only

--- modules/vc/committed.nix
+++ modules/vc/committed.nix
@@ -1,5 +1,11 @@
-{ pkgs, ... }:
 {
+  isLinux,
+  lib,
+  pkgs,
+  ...
+}:
+{ }
+// lib.optionalAttrs isLinux {
   environment.systemPackages = with pkgs; [
     # linting for your git commit messages
     # replaces: commitlint, gitlint
