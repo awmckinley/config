995c9c073b413fae673ad9a235d665c0c7377f0a
Adam McKinley <adam@awmckinley.net>
2025-02-21 19:13:37 -0600

    feat: install curl-impersonate on Linux only

--- modules/web/curl-impersonate.nix
+++ modules/web/curl-impersonate.nix
@@ -1,5 +1,11 @@
-{ pkgs, ... }:
 {
+  isLinux,
+  lib,
+  pkgs,
+  ...
+}:
+{ }
+// lib.optionalAttrs isLinux {
   environment.systemPackages = with pkgs; [
     # special build of curl that can impersonate Chrome & Firefox
     curl-impersonate
