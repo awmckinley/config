4f41e9c9f42063ea0dc827eca88f50e4ae93d20b
Adam McKinley <adam@awmckinley.net>
2025-02-21 18:17:25 -0600

    feat: add MacBook host

--- hosts/molybdenum/default.nix
+++ hosts/molybdenum/default.nix
@@ -1,23 +1,22 @@
 { pkgs, ... }:
 {
   imports = [
-    ./hardware-configuration.nix
-    ../../systems/linux
+    ../../systems/darwin
     ../../users/adam
   ];
 
-  # use KDE desktop
-  desktop = "plasma6";
-
   # machine name
   networking.hostName = "molybdenum";
+
+  # platform where configuration runs
+  nixpkgs.hostPlatform = "aarch64-darwin";
 
   # use the Tokyo Night scheme
   schemeDark = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
   schemeLight = "${pkgs.base16-schemes}/share/themes/tokyo-night-light.yaml";
 
   # first version installed on this machine
-  system.stateVersion = "24.11";
+  system.stateVersion = 6;
 
   # time zone
   time.timeZone = "America/Chicago";
